b1340ae7ebce09e90c90f264fa73ccbe71253ed8
hwolff00 <hwolff00@gmail.com>
2021-04-05 16:45:31 -0400

    Update "metro_parse.py" and "state_parse.py".

--- metro_parse.py
+++ metro_parse.py
@@ -2,27 +2,30 @@ import xlrd
 import sqlite3
 import geopy
 from geopy.geocoders import Nominatim
+import abbreviations
 
 #-------------------------------------------------------------------------------
-#Connect to and open the database file
-conn = sqlite3.connect("metro.sqlite")
+# Add in Abbreviations table if not exists
+abbreviations.add_abbreviations()
+#-------------------------------------------------------------------------------
+# Connect to and open the database file
+conn = sqlite3.connect("THT.sqlite")
 cur = conn.cursor()
 #-------------------------------------------------------------------------------
-#Set up database tables
+# Set up database tables
 cur.executescript('''
     DROP TABLE IF EXISTS Street;
-    DROP TABLE IF EXISTS Metro_Area;
+    DROP TABLE IF EXISTS Metropolitan_Statistica_Area;
 
     CREATE TABLE Street(
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
     complete_street TEXT UNIQUE
     );
 
-    CREATE TABLE Metro_Area(
+    CREATE TABLE  Metropolitan_Statistica_Area(
     id  INTEGER NOT NULL PRIMARY KEY
         AUTOINCREMENT UNIQUE,
-    area TEXT UNIQUE,
-    street_id  INTEGER,
+    name TEXT,
     latitude INTEGER,
     longitude INTEGER,
     vehicle_perc INTEGER,
@@ -31,27 +34,37 @@ cur.executescript('''
     bike_perc INTEGER,
     bike_fatality INTEGER,
     pedestrian_perc INTEGER,
-    ped_fatality INTEGER
+    ped_fatality INTEGER,
+    abbreviation_id INTEGER,
+    street_id  INTEGER
     );
 
 ''')
 #-------------------------------------------------------------------------------
-#Collect and clean the data
+# Create function that will clean certain data
+def cleanup(x):
+    str_x = str(x)[:5]
+    y = float(str_x)
+    return y
+#-------------------------------------------------------------------------------
+# Collect and clean the data
 loc = ("THT_Data_508.xlsx")
 
 # Open workbook and set up index[1] worksheet as object sheet
 workbook = xlrd.open_workbook(loc)
 sheet = workbook.sheet_by_index(1)
 
-#Iterate through the rows of the worksheet
+# Iterate through the rows of the worksheet
 for row in range(sheet.nrows):
 #skip over headers row
     if row != 0:
         metro = sheet.cell_value(row, 0)
-        if metro != "[no data]":
+        if metro != "[no data]" and metro != "Richmond, VA" and metro!= 'Washington-Arlington-Alexandria, DC-VA-MD-WV':
+            # Nominatim doesn't like Richmond, and DC doesn't have a spot in the state abb
                 met = metro.split(',')
                 address = "{},{}".format(met[0], met[1][:3])
-#Place in lat, long coordinates using geopy
+                abbrev_lst = met[1].strip().split("-")
+# Place in lat, long coordinates using geopy
                 try:
                     add = Nominatim(user_agent='THT_data').geocode(address)
                     lat = add.latitude
@@ -62,26 +75,22 @@ for row in range(sheet.nrows):
                     continue
                 else:
                     veh = sheet.cell_value(row, 1) * 100
-                    str_veh = str(veh)[:5]
-                    vehicle = float(str_veh)
+                    vehicle = cleanup(veh)
                 if sheet.cell_value(row, 3) == "[no data]":
                     continue
                 else:
                     pu = sheet.cell_value(row, 3) * 100
-                    str_pub = str(pu)[:5]
-                    pub = float(str_pub)
+                    pub = cleanup(pu)
                 if sheet.cell_value(row, 5) == "[no data]":
                     continue
                 else:
                     bi = sheet.cell_value(row, 5) * 100
-                    str_bike = str(bi)[:5]
-                    bike = float(str_bike)
+                    bike = cleanup(bi)
                 if sheet.cell_value(row, 7) == "[no data]":
                     continue
                 else:
                     pe = sheet.cell_value(row, 7) * 100
-                    str_ped = str(pe)[:5]
-                    ped = float(str_ped)
+                    ped = cleanup(pe)
                 if sheet.cell_value(row, 9) == "[no data]":
                     continue
                 else:
@@ -90,39 +99,40 @@ for row in range(sheet.nrows):
                     continue
                 else:
                     afate = sheet.cell_value(row, 19)
-                    str_afate = str(afate)[:5]
-                    auto_fate = float(str_afate)
+                    auto_fate = cleanup(afate)
                 if sheet.cell_value(row, 21) == "[no data]":
                     continue
                 else:
                     bfate = sheet.cell_value(row, 21)
-                    str_bfate = str(bfate)[:5]
-                    bike_fate = float(str_bfate)
+                    bike_fate = cleanup(bfate)
                 if sheet.cell_value(row, 23) == "[no data]":
                     continue
                 else:
                     pfate = sheet.cell_value(row, 23)
-                    str_pfate = str(pfate)[:5]
-                    ped_fate = float(str_pfate)
-#Sanity/data check
-                print(address, lat, long)
+                    ped_fate = cleanup(pfate)
+# Sanity/data check
+                print(vehicle, lat, long)
 #-------------------------------------------------------------------------------
-#Insert the data into database (still in above loop)
-                cur.execute('''INSERT OR IGNORE INTO Street (complete_street)
-                    VALUES ( ? )''', ( streets, ) )
+# Insert the data into database (still in above loop)
+                for abbrev in abbrev_lst:
+                    cur.execute('''INSERT OR IGNORE INTO Street (complete_street)
+                        VALUES ( ? )''', ( streets, ) )
 
-                cur.execute('SELECT id FROM Street WHERE complete_street = ? ', (streets, ))
-                street_id = cur.fetchone()[0]
+                    cur.execute('SELECT id FROM Street WHERE complete_street = ? ', (streets, ))
+                    street_id = cur.fetchone()[0]
 
-                cur.execute('''INSERT OR REPLACE INTO Metro_Area
-                    (area, street_id, vehicle_perc, latitude, longitude,
-                    public_perc, auto_fatality, bike_perc, bike_fatality,
-                    pedestrian_perc, ped_fatality ) VALUES
-                    ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                    ( metro, street_id, vehicle, lat, long, pub, auto_fate, bike,
-                    bike_fate, ped, ped_fate )
-                    )
+                    cur.execute('SELECT id FROM Abbreviation WHERE name = ? ', (abbrev, ))
+                    abb = cur.fetchone()[0]
+
+                    cur.execute('''INSERT OR REPLACE INTO Metropolitan_Statistica_Area
+                        (name, latitude, longitude, vehicle_perc, public_perc,
+                        auto_fatality, bike_perc, bike_fatality, pedestrian_perc,
+                        ped_fatality, abbreviation_id, street_id ) VALUES
+                        ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                        ( metro, lat, long, vehicle, pub, auto_fate, bike,
+                        bike_fate, ped, ped_fate, abb, street_id )
+                        )
 #-------------------------------------------------------------------------------
-#Save the database file (still in above loop)
-                conn.commit()
+# Save the database file (still in above loops)
+                    conn.commit()
 conn.close()
--- state_parse.py
+++ state_parse.py
@@ -1,45 +1,46 @@
 import sqlite3
 import xlrd
+import abbreviations
 
 #-------------------------------------------------------------------------------
-#Connect/open the database
-conn = sqlite3.connect("state.sqlite")
+# Add in Abbreviations table if not exists
+abbreviations.add_abbreviations()
+#-------------------------------------------------------------------------------
+# Connect/open the database
+conn = sqlite3.connect("THT.sqlite")
 cur = conn.cursor()
 #-------------------------------------------------------------------------------
-#Create the table for our data
+# Create the table for our data
 cur.executescript("""
-DROP TABLE IF EXISTS Road_data;
+DROP TABLE IF EXISTS State;
 
-CREATE TABLE Road_data (
+CREATE TABLE State (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     state TEXT UNIQUE,
-    state_abv TEXT UNIQUE,
-    commute_by_car_percent INTEGER,
-    miles_per_capita INTEGER,
-    seat_belt_percent INTEGER,
-    road_traffic_fatalities INTEGER,
-    dui_fatalities INTEGER
+    car_perc INTEGER,
+    miles_per_cap INTEGER,
+    seat_belt_perc INTEGER,
+    road_fatalities INTEGER,
+    dui_fatalities INTEGER,
+    abbreviation_id INTEGER
  );
 
 """)
 #-------------------------------------------------------------------------------
-#Hard coding way to enter in state ids w/o altering original xlsx
-code_lst = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
+abbrev_lst = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
     "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
     "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
     "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 #-------------------------------------------------------------------------------
 loc = ("THT_Data_508.xlsx")
-#Parse through and lightly clean workbook data and add in state ids
+# Parse through and lightly clean workbook data and add in state ids
 workbook = xlrd.open_workbook(loc)
 sheet = workbook.sheet_by_index(0)
 x = 0
 for row in range(sheet.nrows):
-#Ignore headers row
+# Ignore headers row
     if row != 0:
         states = sheet.cell_value(row, 0)
-        state_id = code_lst[x]
-        x +=1
         int_commute = sheet.cell_value(row, 1) *100
         commute = str(int_commute)[:5]
         int_seat_belt = sheet.cell_value(row, 33) *100
@@ -50,13 +51,19 @@ for row in range(sheet.nrows):
         roadfate = str(int_roadfate)[:4]
         int_dui = sheet.cell_value(row, 11)
         dui = str(int_dui)[:4]
+        abbrev = abbrev_lst[x]
+        x += 1
 
-#Insert the data into the new database
-        cur.execute('''INSERT OR IGNORE INTO Road_data
-            (state, state_abv, commute_by_car_percent, miles_per_capita,
-            seat_belt_percent, road_traffic_fatalities, dui_fatalities )
+# Insert the data into the new database
+        cur.execute('SELECT id FROM Abbreviation WHERE name = ? ', (abbrev, ))
+        abb = cur.fetchone()[0]
+
+        cur.execute('''INSERT OR IGNORE INTO State
+            (state, car_perc, miles_per_cap,
+            seat_belt_perc, road_fatalities, dui_fatalities, abbreviation_id)
             VALUES ( ?, ?, ?, ?, ?, ?, ?)''',
-            (states, state_id, commute, cap, belt, roadfate, dui ) )
+            (states, commute, cap, belt, roadfate, dui, abb) )
 #-------------------------------------------------------------------------------
-#Commit/Save database
-conn.commit()
+# Commit/Save database
+        conn.commit()
+conn.close()
